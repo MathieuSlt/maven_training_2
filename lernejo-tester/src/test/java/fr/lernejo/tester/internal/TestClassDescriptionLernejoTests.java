261405ba14fa957abd5755055cfa296c0a12b0e2
SanjiKush <mathsa007@yahoo.fr>
2021-06-17 12:29:43 +0200

    Reset to old test

--- lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
+++ lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
@@ -3,22 +3,27 @@ package fr.lernejo.tester.internal;
 import fr.lernejo.tester.SomeLernejoTests;
 import fr.lernejo.tester.api.TestMethod;
 
-import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 public class TestClassDescriptionLernejoTests {
 
-    public static void assertEquals(int expect, int got) {
-        if (expect != got)
-            throw new RuntimeException("Assertion failed, expected " + expect + ", got " + got + "!");
+    public static boolean assertEquals(int got, int expected) {
+        return (got == expected);
     }
 
     @TestMethod
-    public void test() {
-        try {
-            var list = new TestClassDescription(SomeLernejoTests.class).listTestMethods();
-            assertEquals(2, list.size());
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+    public static void test() {
+        List<Method> liste = new TestClassDescription(SomeLernejoTests.class).listTestMethods();
+
+        boolean test = assertEquals(liste.size(), 2);
+        System.out.println(test);
+        if (test == false){
+            throw new IllegalStateException("Size of the list is false");
         }
+    }
+
+    public static void main(String[] args) {
+        test();
     }
 }
